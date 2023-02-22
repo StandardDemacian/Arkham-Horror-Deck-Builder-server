13cd1406c402039767480bf538afc9e2314d3222
Michael DiFelice <michaeldifelice@michaels-mbp.myfiosgateway.com>
2023-02-22 17:12:51 -0500

    cors3

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(db, {
 
 const app =express()
 
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `https://standarddemacian.github.io/Arkham-Horror-Deck-Builder-Client/` }))
+// app.use(cors({ origin: process.env.CLIENT_ORIGIN || `https://standarddemacian.github.io/Arkham-Horror-Deck-Builder-Client/` }))
 app.use(cors(`https://standarddemacian.github.io/Arkham-Horror-Deck-Builder-Client/`))
 
 app.use(express.json())
